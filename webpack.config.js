4dbce97f9ccc3456f3977669f33acd5d7f30a504
Elad Tsfany <eladtsfany@gmail.com>
2023-12-20 21:51:16 +0200

    Add module rule for css

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,15 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.bundle.js',
+        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     },
 };
